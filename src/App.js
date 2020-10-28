96ee4cd76df061a4207905905580285b251348cd
JorisOpKdg <joris.gulinck@student.kdg.be>
2020-10-28 16:38:27 +0100

    paginas verder uitgewerkt

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import StartPage from "./components/layout/StartPage";
-import FloorList from "./components/FloorList";
+import RoomsList from "./components/rooms/RoomsList";
+import RoomsMap from "./components/rooms/RoomsMap";
+import RoomDetail from "./components/rooms/RoomDetail";
 import "./App.css";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <body>
+      <>
         <BrowserRouter>
           <Navbar />
           <Switch>
-            <Route path="/floor-list/:id" component={FloorList} />
+            <Route path="/rooms-list/:floorId" component={RoomsList} />
+            <Route path="/rooms-map/:floorId" component={RoomsMap} />
+            <Route path="/room-detail/:floorId/:roomId" component={RoomDetail} />
             <Route path="/" component={StartPage} />
           </Switch>
         </BrowserRouter>
-      </body>
+      </>
     );
   }
 }
