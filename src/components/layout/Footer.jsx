b703d18f34f24a3a6b72b3581daea8cbf996a548
JorisOpKdg <joris.gulinck@student.kdg.be>
2020-10-31 19:35:40 +0100

    lkjkl

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="container-fluid bottom page-footer text-light mt-5 p-5 bg-dark">
+    <footer class="container-fluid bottom page-footer text-light mt-5 p-5 border-top shadow-sm bg-light">
       <p>Copyright 2020 - Domotica app</p>
     </footer>
   );
